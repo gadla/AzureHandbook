523345a3807e8f903cdaa6c438ad4a488826e4e9
Gadi Lev-Ari <gadla@hotmail.com>
2023-09-06 08:53:05 +0300

    Azure Key Vault PS1 file changed, Function logic added and KUSTO query added for auditing search

--- Azure Key Vault/Get-AzureKeyVaultSecret.ps1
+++ Azure Key Vault/Get-AzureKeyVaultSecret.ps1
@@ -28,32 +28,56 @@
         For more information about how to use this function, see the online help: https://github.com/gadla/AzureHandbook/tree/main/Azure%20Key%20Va lt
     #>
 
+
+    [CmdletBinding(PositionalBinding=$false)]
     param(
         [Parameter(Mandatory=$true)]
-        [string]$vaultName,
+        [string]$VaultName,
 
         [Parameter(Mandatory=$true)]
-        [string]$secretName,
+        [string]$SecretName,
 
         [Parameter(Mandatory=$false)]
-        [switch]$asString
+        [switch]$AsString
     )
 
+    # Verifying that the Azure PowerShell module is installed. 
+    $ModuleName = "Az"
+    $InstalledModule = Get-InstalledModule -Name $ModuleName -ErrorAction SilentlyContinue
+    if (-not($installedModule)) {
+        Write-Host "The '$ModuleName' module is not installed."
+        write-host "Please install it from the PowerShell Gallery: https://www.powershellgallery.com/packages/Az/."
+        Write-Host "You can install it by running the following command: Install-Module -Name Az -AllowClobber -Scope CurrentUser"
+        throw "The $ModuleName module is not installed. Please install it from the PowerShell Gallery: https://www.powershellgallery.com/packages/Az/."
+    }
+
+
+    # Verifying the Azure connectivity status within the PowerShell environment.
+    $AzContext = Get-AzContext -ErrorAction SilentlyContinue
+    if ($AzContext) {
+        Write-Verbose "Connected to Azure with context: $($AzContext.Subscription.Name)"
+    } else {
+        Write-Host "Not connected to Azure."
+        throw "You are not connected to Azure. Please connect to Azure using the Connect-AzAccount cmdlet."
+    }
+
     # Check if the Key Vault exists
-    $keyVault = Get-AzKeyVault -VaultName $vaultName -ErrorAction SilentlyContinue
+    Write-Host "Checking if Key Vault $VaultName exists."
+    $keyVault = Get-AzKeyVault -VaultName $VaultName -ErrorAction SilentlyContinue
 
     if ($null -eq $keyVault) {
-        Write-Error "Key Vault $vaultName does not exist."
-        return
+        Write-Host "Key Vault $VaultName was not found. Please ensure the specified Key Vault name is correct, and that you have the necessary permissions to access it. If the Key Vault does not exist, you can create it using the Azure portal or Azure PowerShell."
+        throw "Key Vault $VaultName was not found. Please ensure the specified Key Vault name is correct, and that you have the necessary permissions to access it. If the Key Vault does not exist, you can create it using the Azure portal or Azure PowerShell."
     }
 
     
     # Check if the secret exists
-    $secret = Get-AzKeyVaultSecret -VaultName $vaultName -Name $secretName -ErrorAction SilentlyContinue
+    Write-Host "Checking if secret $secretName exists in Key Vault $VaultName."
+    $secret = Get-AzKeyVaultSecret -VaultName $VaultName -Name $SecretName -ErrorAction SilentlyContinue
 
     if ($null -eq $secret) {
-        Write-Error "Secret $secretName does not exist in Key Vault $vaultName."
-        return $null
+        Write-Host "Secret $SecretName does not exist in Key Vault $VaultName."
+        throw "Secret $SecretName does not exist in Key Vault $VaultName."
     }
 
     if ($asString) {
@@ -72,3 +96,13 @@
 Get-AzKeyVaultSecret -VaultName 'YourVaultName' -InRemovedState
 #Use this command line inorder to recover the secret
 undo-azkeyvaultsecretRemoval -VaultName 'YourVaultName' -Name 'YourDeletedSecretName'
+
+# Use this KUSTO query to get the history of SecretGet operations
+# This example is from https://learn.microsoft.com/en-us/azure/key-vault/general/monitor-key-vault#sample-kusto-queries
+# Remember to remove the # from the beginning of each line
+
+# AzureDiagnostics
+# | where TimeGenerated > ago(90d) 
+# | where ResourceProvider =="MICROSOFT.KEYVAULT" 
+# | where OperationName == "SecretGet"
+# | where requestUri_s contains "Yoav"
